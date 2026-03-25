7d99dbfd1214f68e1f4113d1c369dd3cdd91612c
Minh Pham <minhs2@yahoo.com>
2026-03-25 13:36:08 -0700

    Added boilerplate for MVC repositories & services

--- account-product-manager/src/main/java/com/bank/dashboard/repositories/AccountRepository.java
+++ account-product-manager/src/main/java/com/bank/dashboard/repositories/AccountRepository.java
@@ -1,5 +1,8 @@
 package com.bank.dashboard.repositories;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AccountRepository {
     
 }
--- account-product-manager/src/main/java/com/bank/dashboard/services/AccountService.java
+++ account-product-manager/src/main/java/com/bank/dashboard/services/AccountService.java
@@ -1,5 +1,8 @@
 package com.bank.dashboard.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
     
 }
