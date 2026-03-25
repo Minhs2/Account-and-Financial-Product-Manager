e9d7fc8384e627997cef7a85318cc51634fc9a8d
Minh Pham <minhs2@yahoo.com>
2026-03-25 13:32:56 -0700

    Added boilerplate for MVC controllers

--- account-product-manager/src/main/java/com/bank/dashboard/controllers/EnrollmentController.java
+++ account-product-manager/src/main/java/com/bank/dashboard/controllers/EnrollmentController.java
@@ -1,5 +1,10 @@
 package com.bank.dashboard.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/enrollments")
 public class EnrollmentController {
     
 }
