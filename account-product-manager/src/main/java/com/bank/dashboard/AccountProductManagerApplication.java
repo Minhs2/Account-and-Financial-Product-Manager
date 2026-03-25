32acd54f410bf925022b0121168c7ca05a420c87
Minh Pham <minhs2@yahoo.com>
2026-03-25 12:11:27 -0700

    Spring Initializr: Create a Maven Project Spring Boot version: 3.5.12 Project language: Java Group ID: com.bank.dashboard Artifact ID: account-product-manager Package Name: com.bank.dashboard Packaging Type: 21 Dependencies: None selected (for now)

---
+++ account-product-manager/src/main/java/com/bank/dashboard/AccountProductManagerApplication.java
@@ -0,0 +1,13 @@
+package com.bank.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountProductManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountProductManagerApplication.class, args);
+	}
+
+}
